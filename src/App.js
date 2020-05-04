2bbb4c92cff437b15f1130632b90932e08cc17f8
firefly017 <jui.n.nahar@gmail.com>
2020-05-05 00:02:33 +0200

    three pages initialization idkw

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome home! How are you doing?
-        </a>
-      </header>
+      <Switch>
+        <Route path="/discover" component={DiscoverMoviePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
